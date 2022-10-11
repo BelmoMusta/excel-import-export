d7f9285d76ae8bcda755637ca03292ea5a533349
Mustapha Belmokhtar <belmokhtar.91@gmail.com>
2022-10-11 01:55:08 +0100

    refactoring

--- excel-import-export-processor/src/main/java/io/github/belmomusta/exporter/processor/JavaFileWriter.java
+++ excel-import-export-processor/src/main/java/io/github/belmomusta/exporter/processor/JavaFileWriter.java
@@ -6,31 +6,48 @@ import io.github.belmomusta.exporter.processor.velocity.VelocityWrapper;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.tools.JavaFileObject;
 import java.io.File;
+import java.util.function.BiConsumer;
 
 public class JavaFileWriter {
-    private JavaFileWriter(){
-        throw new IllegalStateException();
-    }
-    public static void writeJavaClassForExcel(ClassWrapper classWrapper, ProcessingEnvironment processingEnv) {
-        try {
-            VelocityWrapper wrapper = WrapperUtils.getVelocityWrapper(classWrapper);
-            final JavaFileObject builderFile = processingEnv.getFiler()
-                    .createSourceFile(classWrapper.getFQNOfGeneratedClass());
-            VelocityGenerator.generateExcelExporterJavaClassFile(wrapper, new File(builderFile.getName()));
-        } catch (Exception e) {
-            throw new JavaFileWriterException(e);
-        }
-    }
-
-    public static void writeJavaClassForCSV(ClassWrapper classWrapper, ProcessingEnvironment processingEnv) {
-        try {
-            VelocityWrapper wrapper = WrapperUtils.getVelocityWrapper(classWrapper);
-            final JavaFileObject cSVExportbuilderFile = processingEnv.getFiler()
-                    .createSourceFile(classWrapper.getFQNOfGeneratedClass());
-            VelocityGenerator.generateCSVExporterJavaClassFile(wrapper, new File(cSVExportbuilderFile.getName()));
-            
-        } catch (Exception e) {
-            throw new JavaFileWriterException(e);
-        }
-    }
+	
+	private JavaFileWriter() {
+		throw new IllegalStateException();
+	}
+	
+	public static void writeJavaClassForExcel(ClassWrapper classWrapper, ProcessingEnvironment processingEnv) {
+		common(classWrapper, processingEnv, JavaFileWriter::excel);
+	}
+	
+	public static void writeJavaClassForCSV(ClassWrapper classWrapper, ProcessingEnvironment processingEnv) {
+		common(classWrapper, processingEnv, JavaFileWriter::csv);
+	}
+	
+	private static void excel(VelocityWrapper wrapper, JavaFileObject builderFile) {
+		try {
+			VelocityGenerator.generateExcelExporterJavaClassFile(wrapper, new File(builderFile.getName()));
+		} catch (Exception e) {
+			throw new JavaFileWriterException(e);
+		}
+	}
+	
+	private static void csv(VelocityWrapper wrapper, JavaFileObject builderFile) {
+		try {
+			VelocityGenerator.generateCSVExporterJavaClassFile(wrapper, new File(builderFile.getName()));
+		} catch (Exception e) {
+			throw new JavaFileWriterException(e);
+		}
+	}
+	
+	private static void common(ClassWrapper classWrapper, ProcessingEnvironment processingEnv,
+							   BiConsumer<VelocityWrapper, JavaFileObject> consumer) {
+		try {
+			VelocityWrapper wrapper = WrapperUtils.getVelocityWrapper(classWrapper);
+			final JavaFileObject builderFile = processingEnv.getFiler()
+					.createSourceFile(classWrapper.getFQNOfGeneratedClass());
+			consumer.accept(wrapper, builderFile);
+			
+		} catch (Exception e) {
+			throw new JavaFileWriterException(e);
+		}
+	}
 }
